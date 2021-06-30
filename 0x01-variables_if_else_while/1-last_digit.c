d3afed1f3b117f5b7254f885c3934ccffd57c749
blacksheepbluez <patchoulibluez@gmail.com>
2021-06-30 16:09:59 -0500

    stars

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
 *main - prints whether or not n greater than 5, less than 6, or 0.
+*return (0);
 */
 int main(void)
-{
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
