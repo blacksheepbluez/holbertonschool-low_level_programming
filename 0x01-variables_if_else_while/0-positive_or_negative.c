bdadc597931bc609cd49da765b36b4bd8a9e2a45
blacksheepbluez <patchoulibluez@gmail.com>
2021-06-30 00:08:53 -0500

    add it up

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-int main(int argc, char *argv[])
+/*
+*
+*Description: this program determines whether the randomizer is posotive or negative
+*Return: returns 0
+*/
+int main(void)
 {
-srand(time(NULL));
-printf("%d\n", rand());
-printf("%d\n", rand());
-printf("%d\n", rand());
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n == 0)
+printf("%d is zero\n", n);
+else if (n > 0)
+printf("%d is negative\n", n);
+else if (n > 0)
+printf("%d is posotive\n", n);
 return (0);
 }
