bebe22d2cd236ef33d93964c42a5f23598ee8bb5
blacksheepbluez <patchoulibluez@gmail.com>
2021-06-30 00:31:26 -0500

    patonthebackbetty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /*
-*
+*main - prints what is said in description.
 *Description: this program determines the randomizer +, =
+*return: always 0.
 */
 int main(void)
 {
@@ -13,7 +14,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-printf("%d is posotive\n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
 else
