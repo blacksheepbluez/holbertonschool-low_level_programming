4dbf6190fc827ba577401479fdd650a8e7c688ee
blacksheepbluez <patchoulibluez@gmail.com>
2021-07-01 09:22:15 -0500

    deuces

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet using the putchar function.
+ *
+ *Return: 0.
+ */
+int mainvoid)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+
