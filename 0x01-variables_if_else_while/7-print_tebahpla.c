185df416f5f1e91f8477eb3c39ae474d785d79a5
blacksheepbluez <patchoulibluez@gmail.com>
2021-07-01 12:18:55 -0500

    zxyLike2fmwrk

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - lowercase alphabet in reverse
+ *Return: 0
+ */
+int main(void)
+{
+char zxy;
+for (zxy = 'a'; zxy <= 'z'; zxy--)
+putchar (zxy);
+putchar ('\n');
+return (0);
+}
