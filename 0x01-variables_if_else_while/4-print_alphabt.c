592136d915d73e63dc2497fd5d9269b56711c9ed
blacksheepbluez <patchoulibluez@gmail.com>
2021-07-01 10:47:35 -0500

    codecheck

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - using putchar function, print the alphabet with all except q and e characters.
+ *
+ *Retern: 0.
+ */
+int main (void)
+{
+char eq;
+for (eq = 'a'; ez <= 'z'; eq++)
+if (eq != 'e' && eq != 'q')
+{
+putchar (eq);
+}
+putchar ('\n');
+return (0);
+}
