7690c2dd1c343721398ae191e73227b0c4a1a071
blacksheepbluez <patchoulibluez@gmail.com>
2021-07-01 10:05:27 -0500

    codelike2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+main - prints the alphabet using the putchar function.
+*
+Return: 0.
+*/
+int main(void)
+{
+char xyz;
+for (xyz = 'a'; xyz <= 'z'; xyz++)
+putchar (xyz);
+for (xyz = 'A'; xyz <= 'A'; xyz++)
+putchar (xyz);
+putchar('\n');
+return (0);
+}
