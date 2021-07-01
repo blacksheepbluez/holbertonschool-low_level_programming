b5822e07345032478f416837a82a5faeec8075ad
blacksheepbluez <patchoulibluez@gmail.com>
2021-07-01 10:06:51 -0500

    edits

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-main - prints the alphabet using the putchar function.
-*
-Return: 0.
+ main - prints the alphabet using the putchar function.
+ *
+ Return: 0.
 */
 int main(void)
 {
