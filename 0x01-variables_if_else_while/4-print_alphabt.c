fba6a40dcfc0ec240b5a007c50dba8392af4c7f9
blacksheepbluez <patchoulibluez@gmail.com>
2021-07-01 10:51:23 -0500

    dotyouri

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
- *main - using putchar function, print the alphabet with all except q and e characters.
+ *main - putchar, print the alphabet with out q and e.
  *
  *Retern: 0.
  */
-int main (void)
+int main(void)
 {
 char eq;
-for (eq = 'a'; ez <= 'z'; eq++)
+for (eq = 'a'; eq <= 'z'; eq++)
 if (eq != 'e' && eq != 'q')
 {
 putchar (eq);
